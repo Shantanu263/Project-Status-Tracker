de945c7e178817a8aedde7fe18e336d83afcc6fd
Shantanu Khopade <shantanu.s.khopade@gmail.com>
2026-01-16 11:03:45 +0530

    Minor fix

--- src/main/java/com/shantanu/projectstatustracker/services/ForgotPasswordService.java
+++ src/main/java/com/shantanu/projectstatustracker/services/ForgotPasswordService.java
@@ -5,5 +5,5 @@ import org.springframework.http.ResponseEntity;
 public interface ForgotPasswordService {
     ResponseEntity<Object> verifyEmail(String email);
     ResponseEntity<Object> verifyOtp(int otp, String email);
-    ResponseEntity<Object> changePassword(String newPassword, Long userId);
+    ResponseEntity<Object> changePassword(String newPassword);
 }
