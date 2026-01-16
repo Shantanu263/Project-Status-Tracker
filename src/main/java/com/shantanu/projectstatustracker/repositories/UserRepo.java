e2ddceb67c809311bf10db9557271236c4a1d4d3
Shantanu Khopade <shantanu.s.khopade@gmail.com>
2026-01-16 10:59:16 +0530

    Fixed the User search query function

--- src/main/java/com/shantanu/projectstatustracker/repositories/UserRepo.java
+++ src/main/java/com/shantanu/projectstatustracker/repositories/UserRepo.java
@@ -1,7 +1,11 @@
 package com.shantanu.projectstatustracker.repositories;
 
 import com.shantanu.projectstatustracker.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +22,17 @@ public interface UserRepo extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByRole_Name(String roleName);
+
+    @Query("""
+       SELECT u FROM User u\s
+       WHERE (
+            LOWER(u.name) LIKE LOWER(CONCAT('%', :search, '%'))
+            OR LOWER(u.email) LIKE LOWER(CONCAT('%', :search, '%'))
+            OR LOWER(u.role.name) LIKE LOWER(CONCAT('%', :search, '%'))
+       )
+      \s""")
+    Page<User> searchUsers(
+            @Param("search") String search,
+            Pageable pageable
+    );
 }
